44d27cb9f5de116b9f41b8a04fe575247739e2c8
Anubhav-code-in-whjr <80476846+Anubhav-code-in-whjr@users.noreply.github.com>
2021-09-09 15:10:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function show() {
+var array_value=[""];
+var pizza=["Chicken Tandoori Pizza", "Deluxe Vegie Pizza"];
+for(var i=0; i<pizza.length; i++);
+array_value= i+array_value+pizza+"<br>";
+console.log(array_value);
+document.getElementById("menu").value;}
